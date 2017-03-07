c9645e2d0e143b01be7ddf8dda39e9413c645a0c
Kristina Karrass <kristinakarrass@gmail.com>
2017-03-07 18:59:59 -0500

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,63 +12,64 @@ game = function() {
 
 		var guessedLetters = [];
 		var lettersUsed = [];
-	for (var i = 0; i < 9; i++) {
+
+	// for (var i = 0; i < 9; i++) {
 
 		document.onkeyup = function(event) {
 
 			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-			// function AddAChild() {
-			// 	var guessedLetters = document.createElement("td");
-			// 	guessedLetters.innerHTML = userGuess;
 
-			// 	var guessedSoFar = document.getElementById("#guessedSoFar");
-			// 	guessedSoFar.appendChild(guessedLetters);
-			// }
-			
-			// AddAChild();
-			lettersUsed.push(userGuess)
-			document.querySelector('#guessesSoFar').innerHTML = lettersUsed; 
 
 
 			if (letters.indexOf(userGuess) !== -1) { //checks that input is a valid letter
 
-			if (userGuess === computerGuess) { 										//user guesses computer's letter
-				alert("You guessed right! You win!");
-				wins++;																//number of wins increased by 1
-				document.querySelector('#wins').innerHTML = wins;					//updates on page
-				document.querySelector('#guessesLeft').innerHTML = 9;				//displays 9 guesses left on page
-				guesses = 9;														//resets guesses to 9
-				game(computerGuess);											 	//game starts over
-			}
+				if (userGuess === computerGuess) { 										//user guesses computer's letter
+					alert("You guessed right! You win!");
+					wins++;																//number of wins increased by 1
+					document.querySelector('#wins').innerHTML = wins;					//updates on page
+					document.querySelector('#guessesLeft').innerHTML = "You have " + 9 + " guesses.";				//displays 9 guesses left on page
+					guesses = 9;														//resets guesses to 9
+					guessedLetters = [];													
+					game(computerGuess);											 	//game starts over
 
-			else if (guesses === 0) {												//all guesses used										
-				losses++;															//number of losses increased by 1
-				document.querySelector("#losses").innerHTML = losses;				//updates on page
-				alert("You loose! The letter was " + computerGuess);				//alerts of loss and states letter chosen by computer
-				document.querySelector('#guessesLeft').innerHTML = 9;				//displays 9 guesses left on page
-				guesses = 9;														//resets guesses to 9		
-				game(computerGuess);												//game starts over
-			}
+				}
 
-			else if (guessedLetters.indexOf(userGuess) !== -1) {					//alerts user if he has already used a letter
-				alert("You have already chosen this letter!");
-			}
+				else if (guessedLetters.indexOf(userGuess) !== -1) {					//alerts user if he has already used a letter
+					alert("You have already chosen this letter!");
+					lettersUsed.pop();
+				}
 
-			else {																	//user does not guess right	
-				guesses--;															//number of guesses minus 1
-				document.querySelector("#guessesLeft").innerHTML = guesses;			//updates nr of guesses on page								
-			}
-			} // closes if statement
-			else {
+				else {																	//user does not guess right	
+					guesses--;															//number of guesses minus 1
+					document.querySelector("#guessesLeft").innerHTML = "You have " + guesses + " guesses.";			//updates nr of guesses on page								
+				}
+	
+
+			guessedLetters.push(userGuess);   //pushes guessed Letters into array for comparison and alert
+			lettersUsed.push(" " + userGuess);
+			document.querySelector('#guessesSoFar').innerHTML = lettersUsed; 
+					}  // closes if statement
+
+			else  {								//user did not choose a letter
 				alert("Please choose a letter.");
 			}
 
-						guessedLetters.push(userGuess);   //pushes guessed Letters into array for comparison and alert
+			if (guesses === 0) {												//all guesses used			
+
+				losses++;															//number of losses increased by 1
+				document.querySelector("#losses").innerHTML = losses;				//updates on page
+				alert("You loose! The letter was " + computerGuess);				//alerts of loss and states letter chosen by computer
+				document.querySelector('#guessesLeft').innerHTML = "You have " + 9 + " guesses.";				//displays 9 guesses left on page
+				guesses = 9;														//resets guesses to 9		
+				game(computerGuess);												//game starts over
+					}
+				
+
 
 		}	//closes document.onkeyup function
 
-	}	// closes for loop
+	// }	// closes for loop
 } // closes function #game
 
 game(computerGuess);
